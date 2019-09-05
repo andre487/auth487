30cc8e2cff8878a2735f98b5507f4f0c3b75de4a
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2019-09-05 23:13:42 +0300

    Actualize deploy

--- lib/auth487/data_handler.py
+++ lib/auth487/data_handler.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import ssl
 
 try:
     import pymongo
@@ -7,14 +8,19 @@ except ImportError as e:
     logging.debug("PyMongo is not presented so don't use data handling")
     pymongo = None
 
-CONNECT_TIMEOUT = 2000
+CONNECT_TIMEOUT = 500
 AUTH_MISTAKES_TO_BAN = int(os.environ.get('AUTH_MISTAKES_TO_BAN', 5))
 AUTH_BAN_TIME = int(os.environ.get('AUTH_BAN_TIME', 86400))
 
 MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
 MONGO_PORT = int(os.environ.get('MONGO_PORT', 27017))
-MONGO_LOGIN = os.environ.get('MONGO_LOGIN')
+
+MONGO_REPLICA_SET = os.environ.get('MONGO_REPLICA_SET')
+MONGO_SSL_CERT = os.environ.get('MONGO_SSL_CERT')
+
+MONGO_USER = os.environ.get('MONGO_USER')
 MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
+MONGO_AUTH_SOURCE = os.environ.get('MONGO_AUTH_SOURCE')
 MONGO_DB_NAME = os.environ.get('AUTH_MONGO_DB_NAME', 'auth487')
 
 _mongo_client = None
@@ -66,12 +72,22 @@ def _get_mongo_client():
 
     logging.info('Connecting to MongoDB: %s:%s', MONGO_HOST, MONGO_PORT)
 
+    mongo_options = dict(
+        connectTimeoutMS=CONNECT_TIMEOUT,
+        authSource=MONGO_DB_NAME,
+    )
+
+    if MONGO_REPLICA_SET:
+        mongo_options['replicaSet'] = MONGO_REPLICA_SET
+
     _mongo_client = pymongo.MongoClient(
         MONGO_HOST, MONGO_PORT,
         connect=True,
-        connectTimeoutMS=CONNECT_TIMEOUT,
-        username=MONGO_LOGIN,
+        username=MONGO_USER,
         password=MONGO_PASSWORD,
+        ssl_ca_certs=MONGO_SSL_CERT,
+        ssl_cert_reqs=ssl.CERT_REQUIRED if MONGO_SSL_CERT else ssl.CERT_NONE,
+        **mongo_options
     )
 
     return _mongo_client
