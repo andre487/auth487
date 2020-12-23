0deb872cbd099050236bff709f11e02b3944ea05
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2020-12-23 15:35:59 +0300

    Implement secrets from Lockbox

---
+++ cli_tasks/prepare_secrets.py
@@ -0,0 +1,48 @@
+import logging
+import os
+from . import common, yandex_cloud
+
+SECRETS = {
+    'auth_info': 'e6q6vfo2vo565h0cb0nq',
+    'mongo': 'e6q0nap2vdn3v6u9r7h8',
+    'gmail_sender': 'e6qar6a7mkjfu29vpv7b',
+}
+
+
+def run(c, recreate_venv, no_secret_cache=False):
+    common.prepare_virtual_env(c, recreate_venv)
+
+    if not no_secret_cache and os.path.exists(common.SECRET_DIR):
+        logging.info('Has secret data, use --no-secret-cache to renew')
+        return
+
+    for secret_name, secret_id in SECRETS.items():
+        secret_data = yandex_cloud.get_secret(secret_id)
+        write_secret_files(secret_name, secret_data)
+
+
+def get_secret_dir():
+    d = common.SECRET_DIR
+    if os.path.exists(d):
+        return d
+
+    os.makedirs(d, mode=0o700)
+    return d
+
+
+def endure_secret_subdir(file_path):
+    d = os.path.dirname(file_path)
+    if os.path.exists(d):
+        return file_path
+
+    os.makedirs(d, mode=0o700)
+    return file_path
+
+
+def write_secret_files(secret_name, secret_data):
+    for file_name, content in secret_data.items():
+        logging.info('Write %s / %s', secret_name, file_name)
+
+        file_path = os.path.join(get_secret_dir(), secret_name, file_name)
+        with open(endure_secret_subdir(file_path), 'wb') as fp:
+            fp.write(content)
