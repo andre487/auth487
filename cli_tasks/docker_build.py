6533e71e1eec537825480aea090a64ab8f08c551
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2023-12-30 23:22:48 +0300

    Get rid of buildx

--- cli_tasks/docker_build.py
+++ cli_tasks/docker_build.py
@@ -7,8 +7,4 @@ def run(_):
     os.chdir(common.PROJECT_DIR)
 
     docker = common.get_docker()
-    subprocess.check_call((
-        docker, 'buildx', 'build', '--platform', 'linux/amd64', '--load',
-        '-t', common.DOCKER_IMAGE_NAME,
-        '--force-rm', '.',
-    ))
+    subprocess.check_call((docker, 'build', '-t', common.DOCKER_IMAGE_NAME, '--force-rm', '.'))
