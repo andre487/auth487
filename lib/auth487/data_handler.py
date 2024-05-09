01fb2e979af61770646a39a04fc4e7add88230cc
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2024-05-09 21:28:31 +0300

    Make Mongo credentials refreshable

--- lib/auth487/data_handler.py
+++ lib/auth487/data_handler.py
@@ -1,40 +1,19 @@
 import logging
 import os
 import secrets
+import typing as tp
 
 import pymongo
 import pyotp
 
 from .common import AUTH_DEV_MODE
 
-
-def get_env_param(name, def_val=None, try_file=False):
-    val = os.getenv(name, def_val)
-
-    if try_file and val and os.path.isfile(val):
-        with open(val) as fp:
-            return fp.read().strip()
-
-    return val
-
-
 CONNECT_TIMEOUT = 500
-AUTH_MISTAKES_TO_BAN = int(get_env_param('AUTH_MISTAKES_TO_BAN', '10'))
-AUTH_BAN_TIME = int(get_env_param('AUTH_BAN_TIME', '60'))
-
-MONGO_HOST = get_env_param('MONGO_HOST', 'localhost', try_file=True)
-MONGO_PORT = int(get_env_param('MONGO_PORT', '27017', try_file=True))
-
-MONGO_REPLICA_SET = get_env_param('MONGO_REPLICA_SET', try_file=True)
-MONGO_SSL_CERT = get_env_param('MONGO_SSL_CERT', try_file=True)
-
-MONGO_USER = get_env_param('MONGO_USER', try_file=True)
-MONGO_PASSWORD = get_env_param('MONGO_PASSWORD', try_file=True)
-MONGO_AUTH_SOURCE = get_env_param('MONGO_AUTH_SOURCE', try_file=True)
-
-MONGO_DB_NAME = get_env_param('MONGO_DB_NAME', 'auth487')
+AUTH_MISTAKES_TO_BAN = int(os.getenv('AUTH_MISTAKES_TO_BAN', '10'))
+AUTH_BAN_TIME = int(os.getenv('AUTH_BAN_TIME', '60'))
 
 _mongo_client = None
+_prev_credentials = None
 
 
 def is_remote_addr_clean(remote_addr):
@@ -96,27 +75,105 @@ def remove_second_factor_record(login, auth_id):
     })
 
 
+class MongoCredentials(tp.NamedTuple):
+    mongo_host: str
+    mongo_port: int
+    mongo_replica_set: str | None
+    mongo_ssl_cert: str | None
+    mongo_user: str | None
+    mongo_password: str | None
+    mongo_auth_source: str | None
+    mongo_db_name: str | None
+    changed: bool | None = None
+
+
+def _get_mongo_db_name() -> str:
+    return _get_env_param('MONGO_DB_NAME', 'auth487')
+
+
+def _get_mongo_credentials() -> MongoCredentials:
+    global _prev_credentials
+
+    mongo_host = _get_env_param('MONGO_HOST', 'localhost')
+    mongo_port = int(_get_env_param('MONGO_PORT', '27017'))
+
+    mongo_replica_set = _get_env_param('MONGO_REPLICA_SET')
+    mongo_ssl_cert = _get_env_param('MONGO_SSL_CERT')
+
+    mongo_user = _get_env_param('MONGO_USER')
+    mongo_password = _get_env_param('MONGO_PASSWORD')
+    mongo_auth_source = _get_env_param('MONGO_AUTH_SOURCE')
+
+    mongo_db_name = _get_mongo_db_name()
+
+    credentials = MongoCredentials(
+        mongo_host,
+        mongo_port,
+        mongo_replica_set,
+        mongo_ssl_cert,
+        mongo_user,
+        mongo_password,
+        mongo_auth_source,
+        mongo_db_name,
+    )
+
+    changed = credentials != _prev_credentials
+    _prev_credentials = credentials
+
+    credentials = list(credentials)
+    credentials[-1] = changed
+
+    return MongoCredentials(*credentials)
+
+
+def _get_env_param(name, def_val=None):
+    val = os.getenv(name, def_val)
+
+    if val and os.path.isfile(val):
+        with open(val) as fp:
+            return fp.read().strip()
+
+    return val
+
+
 def _get_mongo_client():
     global _mongo_client
-    if _mongo_client:
+
+    (
+        mongo_host,
+        mongo_port,
+        mongo_replica_set,
+        mongo_ssl_cert,
+        mongo_user,
+        mongo_password,
+        mongo_auth_source,
+        mongo_db_name,
+        mongo_changed,
+    ) = _get_mongo_credentials()
+
+    if _mongo_client and not mongo_changed:
         return _mongo_client
 
-    logging.info('Connecting to MongoDB: %s:%s', MONGO_HOST, MONGO_PORT)
+    if _mongo_client and mongo_changed:
+        logging.info('Mongo credentials changed, reconnect')
+        _mongo_client.close()
+
+    logging.info('Connecting to MongoDB: %s:%s', mongo_host, mongo_port)
 
     mongo_options = dict(
         connectTimeoutMS=CONNECT_TIMEOUT,
-        authSource=MONGO_DB_NAME,
+        authSource=mongo_db_name,
     )
 
-    if MONGO_REPLICA_SET:
-        mongo_options['replicaSet'] = MONGO_REPLICA_SET
+    if mongo_replica_set:
+        mongo_options['replicaSet'] = mongo_replica_set
 
     _mongo_client = pymongo.MongoClient(
-        MONGO_HOST, MONGO_PORT,
+        mongo_host, mongo_port,
         connect=True,
-        username=MONGO_USER,
-        password=MONGO_PASSWORD,
-        tlsCAFile=MONGO_SSL_CERT,
+        username=mongo_user,
+        password=mongo_password,
+        tlsCAFile=mongo_ssl_cert,
         **mongo_options
     )
 
@@ -125,7 +182,8 @@ def _get_mongo_client():
 
 def _get_second_factor_collection():
     client = _get_mongo_client()
-    collection = client[MONGO_DB_NAME]['second_factor']
+    mongo_db_name = _get_mongo_db_name()
+    collection = client[mongo_db_name]['second_factor']
 
     collection.create_index([
         ('auth_id', pymongo.ASCENDING),
@@ -136,7 +194,8 @@ def _get_second_factor_collection():
 
 def _get_remote_addr_collection():
     client = _get_mongo_client()
-    collection = client[MONGO_DB_NAME]['remote_addresses']
+    mongo_db_name = _get_mongo_db_name()
+    collection = client[mongo_db_name]['remote_addresses']
 
     collection.create_index(
         [('remote_addr', pymongo.ASCENDING)],
