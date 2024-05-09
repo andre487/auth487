44aa3cde0467e638387ad44c5f6be155f37203d2
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2024-05-10 00:29:32 +0300

    Fix entry-point.sh

--- entry-point.sh
+++ entry-point.sh
@@ -4,13 +4,13 @@ cd "$(dirname "$0")"
 
 export PYTHONDONTWRITEBYTECODE=1
 
-if [[ -z "$SECRETS_DIR" ]]; then
+if [[ -z "${SECRETS_DIR:-}" ]]; then
     echo "No secret dir"
     exit 1
 fi
 
 YC_SECRET_RUN_ARG=''
-if [[ "$SECRETS_DEV_RUN" == 1 ]]; then
+if [[ "${SECRETS_DEV_RUN:-}" == 1 ]]; then
     YC_SECRET_RUN_ARG=--dev-run
 fi
 export YC_SECRET_RUN_ARG
