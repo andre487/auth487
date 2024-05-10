bee9bc2717d863703a3882b8bb7210381378c362
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2024-05-10 15:19:33 +0300

    Fix sleep

--- cli_tasks/docker_test.py
+++ cli_tasks/docker_test.py
@@ -12,7 +12,7 @@ def run(c, recreate_venv):
 
     logging.info('Start Docker instance')
     common.start_docker_instance(port, db_name=common.TEST_DB_NAME, as_daemon=True)
-    time.sleep(2)
+    time.sleep(10)
 
     test_proc = subprocess.Popen((common.PYTHON, '-m', 'pytest', '-s', 'http_test.py'), env={
         'APP_PORT': str(port),
