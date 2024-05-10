b714ae298fd952abbf2570b08429cace594f51d9
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2024-05-10 16:11:56 +0300

    Fix ex_file

--- yc_secret_fetcher.py
+++ yc_secret_fetcher.py
@@ -74,6 +74,7 @@ def get_secrets(secrets_dir: str, meta_service: str, dev_run: bool) -> None:
             ex_val = ...
             ex_file = False
             if os.path.exists(sec_file):
+                ex_file = True
                 with open(sec_file, 'rb') as fp:
                     ex_val = fp.read()
 
